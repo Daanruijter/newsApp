bd1efbe000c02f0f47c04a80746f61ed175df0ff
Daan Ruijter <daan_ruijter@hotmail.com>
2020-09-08 18:15:41 +0200

    working on the logic for the random page

--- src/views/RandomPage.vue
+++ src/views/RandomPage.vue
@@ -5,17 +5,20 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import news from "../store/modules/news";
-// import NewsItemType from "../interfacesforapp";
+import NewsItemType from "../interfacesforapp";
 
 @Component
 export default class RandomPage extends Vue {
   newsData = [];
+  randomNewsItem: NewsItemType[] = [];
 
   async mounted() {
     console.log("mounted");
 
     //fetch the newsData and put it in the vuex store
     await news.fetchNews();
+    this.getAllNewsData;
+    this.filterArrayByRandomIndex();
   }
 
   //populate the newsdata data array (necessary to filter based on the newsItemTitle variable)
@@ -25,6 +28,26 @@ export default class RandomPage extends Vue {
     ].newsDataGetter;
 
     return null;
+  }
+
+  //get a random number for the index. Number must be higher than 10, because I don't want to display newsItems that already got displayed on the homepage.
+  //The random number must also not be higher than the length of the array of newsitems
+  randomIntFromInterval(min: number, max: number): number {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  //filter the array with one random index to display a random news item
+  filterArrayByRandomIndex() {
+    const randomNewsItem = this.randomIntFromInterval(10, this.newsData.length);
+    const filterToGetRandomNewsItem: NewsItemType[] = this.newsData.filter(
+      (item: NewsItemType, index: number) => {
+        console.log(randomNewsItem);
+        return index > 9 && index === randomNewsItem;
+      }
+    );
+    this.randomNewsItem = filterToGetRandomNewsItem;
+    console.log(filterToGetRandomNewsItem);
+    return filterToGetRandomNewsItem;
   }
 }
 </script>
